5ce275ea269f0b90833d5b83e0cc687a92b3de91
Sajjad Zaidi <zaidi11c@gmail.com>
2013-10-31 19:25:23 -0400

    redo migrate

--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,11 @@ ActiveRecord::Schema.define(:version => 20131030015133) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "name"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
   end
