fef5646260caf570c57741995f3707a7ba5abbb9
Sajjad Zaidi <zaidi11c@gmail.com>
2014-06-03 07:01:43 -0400

    Delete secret_token.rb

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Bloccit::Application.config.secret_token = '8c9ce0caa44c612727e2013c90164c4510c18b1f498117a9004acd2aeda441503a9e33a806f0efc50aaa668af23d3965b5083e0595ac1021a7698c74551cf554'
